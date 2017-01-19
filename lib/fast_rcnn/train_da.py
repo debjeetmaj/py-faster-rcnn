637aac41e023c1091810c7cca7a3adab3d7f4052
Debjeet <debjeetmaj17@gmail.com>
2017-01-19 19:24:57 +0530

    Added tools to train adapation network

---
+++ lib/fast_rcnn/train_da.py
@@ -0,0 +1,68 @@
+# --------------------------------------------------------
+# Fast R-CNN
+# Copyright (c) 2015 Microsoft
+# Licensed under The MIT License [see LICENSE for details]
+# Written by Ross Girshick
+# --------------------------------------------------------
+
+"""Train a Fast R-CNN network."""
+
+import caffe
+from fast_rcnn.config import cfg
+from train import SolverWrapper,filter_roidb
+import roi_data_layer.roidb as rdl_roidb
+from utils.timer import Timer
+import numpy as np
+import os
+
+from caffe.proto import caffe_pb2
+import google.protobuf as pb2
+
+class DASolverWrapper(SolverWrapper):
+    """A simple wrapper around Caffe's solver.
+    This wrapper gives us control over the snapshotting process, which we
+    use to unnormalize the learned bounding-box regression weights.
+    """
+    def __init__(self, solver_prototxt, src_roidb,target_roidb, output_dir,
+                 pretrained_model=None):
+        """Initialize the SolverWrapper."""
+        self.output_dir = output_dir
+
+        if (cfg.TRAIN.HAS_RPN and cfg.TRAIN.BBOX_REG and
+            cfg.TRAIN.BBOX_NORMALIZE_TARGETS):
+            # RPN can only use precomputed normalization because there are no
+            # fixed statistics to compute a priori
+            assert cfg.TRAIN.BBOX_NORMALIZE_TARGETS_PRECOMPUTED
+
+        if cfg.TRAIN.BBOX_REG:
+            print 'Computing bounding-box regression targets...'
+            self.bbox_means, self.bbox_stds = \
+                    rdl_roidb.add_bbox_regression_targets(src_roidb)
+            print 'done'
+
+        self.solver = caffe.SGDSolver(solver_prototxt)
+        if pretrained_model is not None:
+            print ('Loading pretrained model '
+                   'weights from {:s}').format(pretrained_model)
+            self.solver.net.copy_from(pretrained_model)
+
+        self.solver_param = caffe_pb2.SolverParameter()
+        with open(solver_prototxt, 'rt') as f:
+            pb2.text_format.Merge(f.read(), self.solver_param)
+        
+        
+        self.solver.net.layers[0].set_roidb(src_roidb,target_roidb)
+
+def train_net(solver_prototxt, src_roidb, target_roidb, output_dir,
+              pretrained_model=None, max_iters=40000):
+    """Train a Fast R-CNN network."""
+
+    src_roidb = filter_roidb(src_roidb)
+    # target_roidb = filter_roidb(target_roidb)
+    sw = DASolverWrapper(solver_prototxt, src_roidb,target_roidb, output_dir,
+                       pretrained_model=pretrained_model)
+
+    print 'Solving...'
+    model_paths = sw.train_model(max_iters)
+    print 'done solving'
+    return model_paths
---
+++ tools/train_da_net.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+
+#--------------------------------------------------------
+# Domain Adaptation
+# Written By Debjeet Majumdar
+#--------------------------------------------------------
+
+import _init_paths
+from fast_rcnn.train import get_training_roidb
+from fast_rcnn.train_da import train_net
+from fast_rcnn.config import cfg, cfg_from_file, cfg_from_list, get_output_dir
+from datasets.factory import get_imdb
+import datasets.imdb
+import caffe
+import argparse
+import pprint
+import numpy as np
+import sys
+from train_net import combined_roidb 
+
+def parse_args():
+    """
+    Parse input arguments
+    """
+    parser = argparse.ArgumentParser(description='Train a Fast R-CNN network')
+    parser.add_argument('--gpu', dest='gpu_id',
+                        help='GPU device id to use [0]',
+                        default=0, type=int)
+    parser.add_argument('--solver', dest='solver',
+                        help='solver prototxt',
+                        default=None, type=str)
+    parser.add_argument('--iters', dest='max_iters',
+                        help='number of iterations to train',
+                        default=40000, type=int)
+    parser.add_argument('--weights', dest='pretrained_model',
+                        help='initialize with pretrained model weights',
+                        default=None, type=str)
+    parser.add_argument('--cfg', dest='cfg_file',
+                        help='optional config file',
+                        default=None, type=str)
+    parser.add_argument('-s','--srcimdb', dest='src_imdb_name',
+                        help='dataset to train on',
+                        default='voc_2007_trainval', type=str)
+    parser.add_argument('-t','--tgtimdb', dest='target_imdb_name',
+                        help='dataset to train on',
+                        default='minicoco_2014_train', type=str)
+    parser.add_argument('--rand', dest='randomize',
+                        help='randomize (do not use a fixed seed)',
+                        action='store_true')
+    parser.add_argument('--set', dest='set_cfgs',
+                        help='set config keys', default=None,
+                        nargs=argparse.REMAINDER)
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(1)
+
+    args = parser.parse_args()
+    return args
+
+def add_domain_label(roidb,domain):
+    """
+    Adds a domain label to all roidbs
+        Source : 0
+        Target : 1
+    """
+    for roi in roidb:
+        roi['domain']=domain
+
+def combined_target_roidb(imdb_name):
+    imdb = get_imdb(imdb_name)
+    print 'Loaded dataset `{:s}` for training'.format(imdb.name)
+    PROPOSAL_METHOD = 'target'
+    imdb.set_proposal_method(PROPOSAL_METHOD)
+    print 'Set proposal method: {:s}'.format(PROPOSAL_METHOD)
+    roidb = get_training_roidb(imdb)
+    return imdb,roidb
+
+if __name__ == '__main__':
+    args = parse_args()
+    print('Called with args:')
+    print(args)
+
+    if args.cfg_file is not None:
+        cfg_from_file(args.cfg_file)
+    if args.set_cfgs is not None:
+        cfg_from_list(args.set_cfgs)
+
+    cfg.GPU_ID = args.gpu_id
+
+    print('Using config:')
+    pprint.pprint(cfg)
+
+    if not args.randomize:
+        # fix the random seeds (numpy and caffe) for reproducibility
+        np.random.seed(cfg.RNG_SEED)
+        caffe.set_random_seed(cfg.RNG_SEED)
+
+    # set up caffe
+    caffe.set_mode_gpu()
+    caffe.set_device(args.gpu_id)
+
+    target_imdb,target_roidb = combined_target_roidb(args.target_imdb_name)
+    print '{:d} target roidb entries'.format(len(target_roidb))
+    add_domain_label(target_roidb,1)
+
+    src_imdb, src_roidb = combined_roidb(args.src_imdb_name)
+    print '{:d} source roidb entries'.format(len(src_roidb))
+    add_domain_label(src_roidb,0)
+
+    # #merge roidbs
+    # roidb = merge_roidbs(src_roidb,target_roidb)
+    # print '{:d} Total roidb entries'.format(len(roidb))
+    
+    output_dir = get_output_dir(target_imdb)
+    print 'Output will be saved to `{:s}`'.format(output_dir)
+
+    train_net(args.solver, src_roidb, target_roidb, output_dir,
+                pretrained_model=args.pretrained_model,
+                max_iters=args.max_iters)
